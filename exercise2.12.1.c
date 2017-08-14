9109a33a82271a275d562de27c2812d9e84e2ffa
GYF98 <GYF98@users.noreply.github.com>
2017-08-14 10:40:44 +0800

    Exercise 8.14

---
+++ exercise2.12.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int years, days;
+	years = 18;
+	days = years * 365;
+	
+	printf("I am %d years old and I lived in the world for %d days", years, days);
+
+	return 0;
+
+
+}
