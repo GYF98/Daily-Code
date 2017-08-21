d14dc6069d868c0e43a9f539a5428ff9d3f8f965
GYF98 <GYF98@users.noreply.github.com>
2017-08-21 11:10:32 +0800

    Tired

---
+++ fuck.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void fuck(void);
+int main(void)
+{
+	printf("I don't want to go to college!!!\n");
+	fuck();
+	fuck();
+	fuck();
+	fuck();
+	fuck();
+	fuck();
+	fuck();
+	return 0;
+}
+
+void fuck(void)
+{
+	printf("fuck!\nfuck!\nfuck!\nfuck!\nfuck!\nfuck!\nfuck!\n");
+}
