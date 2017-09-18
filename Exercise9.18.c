d7bb7c41e8c153d3dd35167255e5629ef244a2f5
GYF98 <GYF98@users.noreply.github.com>
2017-09-18 13:13:28 +0800

    Exercise9.18

---
+++ Exercise9.18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int num1, num2, a, b, c, d, e;
+	scanf("%d%d", &num1, &num2);
+	a = num1 + num2;
+	b = num1 - num2;
+	c = num1*num2;
+	d = num1/num2;
+	e = num1%num2;
+	printf("%d %d %d %d %d", a, b, c, d, e);
+
+
+}
