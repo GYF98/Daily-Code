be5469991a8f8bd2c17c2a68e035e55fdca094db
GYF98 <GYF98@github.com>
2019-03-03 14:38:59 +0800

    Java 19.3.3

---
+++ TestDataMaxMin.java
@@ -0,0 +1,12 @@
+public class TestDataMaxMin {
+
+public static void main(String[] args) {
+    System.out.println("byte取值范围："+Byte.MIN_VALUE+"至"+Byte.MAX_VALUE);
+    System.out.println("short取值范围："+Short.MIN_VALUE+"至"+Short.MAX_VALUE);
+    System.out.println("int取值范围："+Integer.MIN_VALUE+"至"+Integer.MAX_VALUE);
+    System.out.println("long取值范围："+Long.MIN_VALUE+"至"+Long.MAX_VALUE);
+    System.out.println("float取值范围："+Float.MIN_VALUE+"至"+Float.MAX_VALUE);
+    System.out.println("double取值范围："+Double.MIN_VALUE+"至"+Double.MAX_VALUE);
+
+    }
+}
